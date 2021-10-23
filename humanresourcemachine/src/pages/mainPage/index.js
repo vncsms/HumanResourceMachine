384f6637bff62906db8e15c71d3d27a5956fb8e4
Vinícius Morais Silva <vmsabc@gmail.com>
2021-10-23 19:02:39 -0300

    WIP

--- humanresourcemachine/src/pages/mainPage/index.js
+++ humanresourcemachine/src/pages/mainPage/index.js
@@ -3,23 +3,37 @@ import {
   add,
   sub
 } from '../../components/functions';
+import styles from './style.module.css';
 
 export default class MainPage extends Component {
 
   constructor() {
     super();
     this.state = {
-      commands: ['INBOX', 'COPYTO 1', 'ADD 1', 'COPYTO 3', 'SUB 3'],
+      commands: [
+        'INBOX',
+        'COPYTO 1',
+        'ADD 1',
+        'COPYTO 3',
+        'COPYFROM 1',
+        'OUTBOX',
+        'SUB 3',
+        'a:',
+        'SUB 3',
+        'BUMP+ 3',
+        'BUMP- 1',
+        'JUMP a',
+        'OUTBOX'
+      ],
+      labels:              {
+        
+      },
       offSet: 0,
-      memory: [],
+      memory: new Array(20).fill(null),
       ram: null,
       inbox: [1,2,3,4,5,6,7,8,9],
-      outbos: [],
+      outbox: [],
     };
-  }
-
-  componentDidMount() {
-    this.setState({memory: new Array(5).fill(0)});
   }
 
   checkCommand(cmd) {
@@ -28,9 +42,26 @@ export default class MainPage extends Component {
     return commandAndValue;
   }
 
+  renderTable(data) {
+    return (
+      <div className={styles.table}>
+        {
+          data.map((item) => (
+            <div className={styles.box}>
+              <div className={styles.innerBox}>{item}</div>
+            </div>
+          ))
+        }
+      </div>
+    )
+  }
+
   nextCommand() {
 
-    console.log(this.state.inbox);
+    if(this.state.offSet >= this.state.commands.length) {
+      console.log('fim');
+      return null;
+    }
 
     const cmd = this.state.commands[this.state.offSet];
     console.log(cmd);
@@ -38,56 +69,119 @@ export default class MainPage extends Component {
 
     let result = null;
 
+    const mem = [...this.state.memory];
+
     switch(cv[0]) {
       case 'ADD':
         result = add(this.state.ram, this.state.memory[parseInt(cv[1])]);
         this.setState({ram: result});
+        this.setState({ offSet: this.state.offSet + 1 });
+        break;
+      case 'BUMPUP':
+        mem[parseInt(cv[1])] += 1;
+        // result = add(1, mem[parseInt(cv[1])]);
+        this.setState({ram: result, memory: mem});
+        this.setState({ offSet: this.state.offSet + 1 });
+        break;
+      case 'BUMPDN':
+        mem[parseInt(cv[1])] -= 1;
+        // result = sub(mem[parseInt(cv[1])], 1);
+        this.setState({ram: result, memory: mem});
+        this.setState({ offSet: this.state.offSet + 1 });
         break;
       case 'SUB':
         result = sub(this.state.ram, this.state.memory[parseInt(cv[1])]);
         this.setState({ram: result});
+        this.setState({ offSet: this.state.offSet + 1 });
         break;
       case 'COPYTO':
-        const mem = [...this.state.memory];
         mem[parseInt(cv[1])] = this.state.ram;
         this.setState({memory: mem});
+        this.setState({ offSet: this.state.offSet + 1 });
+        break;
+      case 'COPYFROM':
+        this.setState({ram: this.state.memory[parseInt(cv[1])]});
+        this.setState({ offSet: this.state.offSet + 1 });
         break;
       case 'INBOX':
         const ib = [...this.state.inbox];
         const value = ib.shift();
         this.setState({ inbox: ib, ram: value });
+        this.setState({ offSet: this.state.offSet + 1 });
+        break;
+      case 'OUTBOX':
+        const ob = [...this.state.outbox];
+        ob.unshift(this.state.ram);
+        this.setState({ outbox: ob });
+        this.setState({ offSet: this.state.offSet + 1 });
+        break;
+      case 'JUMP':
+        this.setState({offSet: parseInt(cv[1])})
+        break;
+      case 'JUMPZ':
+        if(this.state.ram === 0) {
+          this.setState({offSet: parseInt(cv[1])})
+        }
+        break;
+      case 'JUMPN':
+        if(this.state.ram < 0) {
+          this.setState({offSet: parseInt(cv[1])})
+        }
+        break;
       default:
-        console.log('+' + cv[0] + '+');
-        console.log("NENHUM DOS DOIS");
+        console.log("NENHUM CASO");
     }
-
-    this.setState({ offSet: this.state.offSet + 1 });
   }
 
   render() {
 
     return (
-      <div>
+      <div style={{
+          backgroundColor: 'blue',
+          width: '1000px',
+          height: '800px',
+          position: 'relative',}}>
         <button onClick={() => this.nextCommand()}>
           next command
         </button>
-        <br/>inbox:
-        <div style={{display: 'flex', flexDirection: 'row'}}>
-        {
-          this.state.inbox.map((item) => (
-            <div style={{border: '1px solid black', maxWidth: '20px', padding: '2px'}}>{item}</div>
-          ))
-        }
+        <div style={{position: 'absolute', bottom: 0, left: '10px'}}>
+          {this.renderTable(this.state.inbox)}
         </div>
-        <div style={{display: 'flex', flexDirection: 'row'}}>
-        ram: <div style={{border: '1px solid black', maxWidth: '20px', padding: '2px'}}>{this.state.ram}</div>
+        <div style={{
+          display: 'flex',
+          width: 400,
+          padding: '10px',
+          backgroundColor: 'violet',
+          justifyContent: "center",
+          flexWrap: "wrap",
+          position: "absolute",
+          left: "290px",
+          top: "200px",
+        }}>
+          {
+            this.state.memory.map((item, index) =>
+              (
+                <div className={styles.memory}>
+                  { item || item === 0 ? 
+                      (
+                        <div className={styles.box}>
+                          <div className={styles.innerBox}>{item}</div>
+                        </div>
+                      )
+                    : null}
+                  <div style={{position: 'absolute', bottom: 0}}>{index}</div>
+                </div>
+              )
+            )
+          }
         </div>
-        <div style={{display: 'flex', flexDirection: 'row'}}>memory:
-        {
-          this.state.memory.map((item) => (
-            <div style={{border: '1px solid black', maxWidth: '20px', padding: '2px'}}>{item}</div>
-          ))
-        }
+        <div style={{position: 'absolute', bottom: 0, right: '10px'}}>
+          {this.renderTable(this.state.outbox)}
+        </div>
+        <div style={{position: 'absolute', top: 0, right: '10px'}}>
+          <div className={styles.box}>
+            <div className={styles.innerBox}>{this.state.ram}</div>
+          </div>
         </div>
       </div>       
     );
